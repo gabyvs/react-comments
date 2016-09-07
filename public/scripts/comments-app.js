68ac7d670f23c47a06367a8aaee7ae711420a45b
Gabriela Vazquez <gabs.vz@gmail.com>
2016-09-06 19:34:03 -0700

    Saving new comments using the form.

--- public/scripts/comments-app.js
+++ public/scripts/comments-app.js
@@ -36,6 +36,20 @@ var CommentBox = React.createClass({
             }.bind(this)
         });
     },
+    handleCommentSubmit: function (comment) {
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            type: 'POST',
+            data: comment,
+            success: function (data) {
+                this.setState({ data: data });
+            }.bind(this),
+            error: function (xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
     // This function executes exactly once during the lifecycle of a component
     getInitialState: function () {
         return { data: [] };
@@ -50,7 +64,7 @@ var CommentBox = React.createClass({
             <div className="commentBox">
                 <h1>Comments</h1>
                 <CommentList data={ this.state.data }/>
-                <CommentForm />
+                <CommentForm onCommentSubmit={ this.handleCommentSubmit } />
             </div>
         );
     }
@@ -81,11 +95,31 @@ var CommentList = React.createClass({
  * So far, comment form is only a div
  */
 var CommentForm = React.createClass({
+    getInitialState: function () {
+        return { author: '', text: '' };
+    },
+    handleAuthorChange: function (e) {
+        this.setState({ author: e.target.value });
+    },
+    handleTextChange: function (e) {
+        this.setState({ text: e.target.value });
+    },
+    handleSubmit: function (e) {
+        e.preventDefault(); // do not reload everything :)
+        var author = this.state.author.trim();
+        var text = this.state.text.trim();
+        if (!text || !author) { return; }
+        this.props.onCommentSubmit({ author: author, text: text });
+        this.setState({ author: '', text: '' });
+    },
+    // Note lowerCamelCase on events
     render: function () {
         return (
-            <div className="commentForm">
-                Hello World! I'm a comment form
-            </div>
+            <form className="commentForm" onSubmit={ this.handleSubmit }>
+                <input type="text" placeholder="Your name" value={ this.state.author } onChange={ this.handleAuthorChange } />
+                <input type="text" placeholder="Say something..." value={ this.state.text } onChange={ this.handleTextChange } />
+                <input type="submit" value="Post" />
+            </form>
         );
     }
 });
